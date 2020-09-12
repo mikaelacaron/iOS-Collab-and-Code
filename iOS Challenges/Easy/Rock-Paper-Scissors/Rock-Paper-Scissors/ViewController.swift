76859a9005a8f8146dae65b9c96b8ffba3f37cdd
Mikaela Caron <mikaelacaron@gmail.com>
2020-09-12 17:17:02 -0400

    Add Easy iOS Starter Project and Sketch file
    Sketch is the UI of the project for the iOS Challenge

---
+++ iOS Challenges/Easy/Rock-Paper-Scissors/Rock-Paper-Scissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Mikaela Caron on 9/12/20.
+//  Copyright © 2020 Mikaela Caron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
