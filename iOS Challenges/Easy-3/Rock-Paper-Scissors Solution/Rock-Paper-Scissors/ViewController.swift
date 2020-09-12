fe5cede2e742b5228d33738b1b679cf7f4fdd808
Mikaela Caron <mikaelacaron@gmail.com>
2020-09-12 18:02:15 -0400

    Add iOS Challenge Easy-3

---
+++ iOS Challenges/Easy-3/Rock-Paper-Scissors Solution/Rock-Paper-Scissors/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Mikaela Caron on 9/12/20.
+//  Copyright © 2020 Mikaela Caron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let rpsChoices = ["✊", "🖐", "✌️"]
+    var randomNumber = 0
+    
+    @IBOutlet var choiceLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func playButtonTapped(_ sender: Any) {
+    }
+    
+}
+
